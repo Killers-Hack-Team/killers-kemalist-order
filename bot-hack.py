1a004dec04d3c25f1edbddd2f05d5f96c31762c1
killers <71911485+Killers-Hack-Team@users.noreply.github.com>
2021-11-25 23:52:25 +0300

    Add files via upload

---
+++ bot-hack.py
@@ -0,0 +1,27 @@
+import pyautogui
+import time
+import keyboard
+time.sleep(3)
+while True:
+    if keyboard.is_pressed('q'):
+        quit()
+    pyautogui.write("owoh")
+    pyautogui.press("enter")
+    time.sleep(7.31)
+    pyautogui.write("owoh")
+    pyautogui.press("enter")
+    time.sleep(7.31)
+    pyautogui.write("owoh")
+    pyautogui.press("enter")
+    time.sleep(7.31)
+    pyautogui.write("owoh")
+    pyautogui.press("enter")
+    time.sleep(7.31)
+    
+    
+    
+    
+    pyautogui.press("enter")
+    pyautogui.write("owo sell all")
+    pyautogui.press("enter")
+    time.sleep(7.31)
